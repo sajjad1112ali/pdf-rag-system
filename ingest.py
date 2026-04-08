7c36be382695752a76e2dc44f2573a4e7fd3299c
Sajjad Ali <sajjadramzan1211@gmail.com>
2026-04-08 12:52:20 +0500

    feat(pdf): text divided into chunks

--- ingest.py
+++ ingest.py
@@ -1,4 +1,5 @@
-import fitz  # PyMuPDF
+import fitz
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def extract_text_from_pdf(pdf_path):
@@ -12,10 +13,27 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
+def chunk_text(text):
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    chunks = text_splitter.split_text(text)
+
+    return chunks
+
+
 if __name__ == "__main__":
+
     pdf_path = "data/sample.pdf"
 
     extracted_text = extract_text_from_pdf(pdf_path)
 
-    print("\n----- EXTRACTED TEXT -----\n")
-    print(extracted_text[:1000])  # print first 1000 characters
+    chunks = chunk_text(extracted_text)
+
+    print("\nTotal Chunks:", len(chunks))
+
+    print("\n--- FIRST CHUNK ---\n")
+    print(chunks[0])
