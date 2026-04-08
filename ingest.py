9532529dab1b6e1487ed8f75bd5a358b68911fbd
Sajjad Ali <sajjadramzan1211@gmail.com>
2026-04-08 13:14:01 +0500

    feat(pdf): text to embedding

--- ingest.py
+++ ingest.py
@@ -1,5 +1,6 @@
 import fitz
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
 
 
 def extract_text_from_pdf(pdf_path):
@@ -25,6 +26,15 @@ def chunk_text(text):
     return chunks
 
 
+def create_embeddings(chunks):
+
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    embeddings = model.encode(chunks)
+
+    return embeddings
+
+
 if __name__ == "__main__":
 
     pdf_path = "data/sample.pdf"
@@ -33,7 +43,11 @@ if __name__ == "__main__":
 
     chunks = chunk_text(extracted_text)
 
+    embeddings = create_embeddings(chunks)
+
     print("\nTotal Chunks:", len(chunks))
 
-    print("\n--- FIRST CHUNK ---\n")
-    print(chunks[0])
+    print("\nEmbedding Vector Length:", len(embeddings[0]))
+
+    print("\nFirst Embedding Vector (first 10 values):")
+    print(embeddings[0][:10])
